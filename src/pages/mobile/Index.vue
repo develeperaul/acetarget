e0fc5af5a959d983300ef2f222972cea28269264
Ralfik <developeraul@gmail.com>
2022-01-21 08:07:25 +0500

    инициализация проекта

---
+++ src/pages/mobile/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page padding class="flex flex-center text-center">
+    <q-spinner
+      color="primary"
+      size="6em"
+      :thickness="10"
+    />
+    <!-- МАКЕТ ПРИЛОЖЕНИЯ ACE-TARGET<br>
+    Слева доступно выезжающее меню, которое имеет ссылки на макеты
+    <original-button
+      class="q-mt-xs"
+      color="red-2"
+      @click="(btn) => leftDrawerOpen(btn)">
+      Открыть меню
+    </original-button>> -->
+  </q-page>
+</template>
+
+<script>
+// import OriginalButton from 'src/components/OriginalButton.vue'
+export default {
+  components: {
+    // OriginalButton
+  },
+  name: 'PageIndex',
+  data: () => ({
+  }),
+  beforeMount () {
+    // this.$router.push('auth')
+  },
+  methods: {
+    leftDrawerOpen (btn) {
+      btn.offLoad()
+      this.$root.$emit('leftDrawerOpen', true)
+    }
+  }
+}
+</script>
